ee97475ce214b34e90ecad72234f60c9736adb65
Johannes Eslage <hello@johanneseslage.de>
2019-04-06 03:03:46 +0200

    Added gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,19 @@ module.exports = {
         name: 'markdown-pages'
       }
     },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Pelican Bar',
+        short_name: 'Pelican',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#a2466c',
+        display: 'standalone'
+        // icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        // include_favicon: true // Include favicon
+      }
+    },
     'gatsby-transformer-remark',
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet'

ec0cb53eb3bfcabe59e2a0c168aebf50423e674d
Johannes Eslage <hello@johanneseslage.de>
2019-04-20 12:48:25 +0200

    Console log for debugging

--- src/cms/preview/static.jsx
+++ src/cms/preview/static.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { StaticPageTemplate } from '../../components/templates/static';
 
-const StaticPreview = ({ entry, widgetFor }) => (
-  <StaticPageTemplate html={widgetFor('body')} headline={entry.getIn(['data', 'headline'])} />
-);
+const StaticPreview = ({ entry, widgetFor }) => {
+  console.log(widgetFor('body'));
+  return (
+    <StaticPageTemplate html={widgetFor('body')} headline={entry.getIn(['data', 'headline'])} />
+  );
+};
 
 export default StaticPreview;

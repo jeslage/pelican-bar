d67b07efa4d6a8c1b2c0413d5d77958ad1542a3a
johanneseslage <johannes.eslage@fork.de>
2019-06-23 15:32:16 +0200

    Added manifest icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,8 +36,50 @@ module.exports = {
         background_color: '#f7f0eb',
         theme_color: '#a2466c',
         display: 'standalone',
-        icon: `static/icon.png`, // This path is relative to the root of the site.
-        include_favicon: true // Include favicon
+        icons: [
+          {
+            src: `/icon-72x72`,
+            sizes: `72x72`,
+            type: `image/png`
+          },
+          {
+            src: `/icon-96x96`,
+            sizes: `96x96`,
+            type: `image/png`
+          },
+          {
+            src: `/icon-128x128`,
+            sizes: `128x128`,
+            type: `image/png`
+          },
+
+          {
+            src: `/icon-144x144`,
+            sizes: `144x144`,
+            type: `image/png`
+          },
+          {
+            src: `/icon-152x152`,
+            sizes: `152x152`,
+            type: `image/png`
+          },
+          {
+            src: `/icon-192x192`,
+            sizes: `192x192`,
+            type: `image/png`
+          },
+          {
+            src: `/icon-384x384`,
+            sizes: `384x384`,
+            type: `image/png`
+          },
+          {
+            src: `/icon-512x512`,
+            sizes: `512x512`,
+            type: `image/png`
+          }
+        ],
+        include_favicon: true
       }
     },
     'gatsby-plugin-styled-components',
